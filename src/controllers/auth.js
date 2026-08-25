95a56b8ef7feae01c0862c9fcc8c5e572f67e6e7
jacQUE56 <jamestonibor65@gmail.com>
2026-08-26 00:54:13 +0100

    fix: update sameSite attribute in COOKIE_OPTIONS for production environment

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,7 +4,7 @@ const { signUp, logIn, issueSessionToken: issuePasswordSessionToken } = require(
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'Lax',
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
